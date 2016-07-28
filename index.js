5c1e819c786d97aae90cc8fa98143efdb49fa352
Alex Boquist <aplchian@MacBook-Pro.local>
2016-07-28 17:54:02 -0400

    env type

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get("/", function(req, res){
 	res.send(JSON.stringify(userInfo));
 })
 
-app.set('port', (process.enc.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function(){
 	console.log('Node app is now running on port', app.get('port'));
